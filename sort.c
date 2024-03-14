413a56cee3bfe7d910a2c2680ad3ad4b8fc2dca7
Brendan Rodriguez <br712811@ucf.edu>
2024-03-14 12:04:42 -0400

    added merge and completed a mergeSort function.

--- sort.c
+++ sort.c
@@ -27,10 +27,79 @@ size_t Size(void* ptr)
 	return ((size_t*)ptr)[-1];
 }
 
+// Merges two subarrays of arr[]. 
+// First subarray is arr[l..m] 
+// Second subarray is arr[m+1..r] 
+void merge(int arr[], int l, int m, int r) 
+{ 
+    int i, j, k; 
+    int n1 = m - l + 1; 
+    int n2 = r - m; 
+  
+    // Create temp arrays 
+    int L[n1], R[n2]; 
+  
+    // Copy data to temp arrays 
+    // L[] and R[] 
+    for (i = 0; i < n1; i++) 
+        L[i] = arr[l + i]; 
+    for (j = 0; j < n2; j++) 
+        R[j] = arr[m + 1 + j]; 
+  
+    // Merge the temp arrays back 
+    // into arr[l..r] 
+    // Initial index of first subarray 
+    i = 0; 
+  
+    // Initial index of second subarray 
+    j = 0; 
+  
+    // Initial index of merged subarray 
+    k = l; 
+    while (i < n1 && j < n2) { 
+        if (L[i] <= R[j]) { 
+            arr[k] = L[i]; 
+            i++; 
+        } 
+        else { 
+            arr[k] = R[j]; 
+            j++; 
+        } 
+        k++; 
+    } 
+  
+    // Copy the remaining elements 
+    // of L[], if there are any 
+    while (i < n1) { 
+        arr[k] = L[i]; 
+        i++; 
+        k++; 
+    } 
+  
+    // Copy the remaining elements of 
+    // R[], if there are any 
+    while (j < n2) { 
+        arr[k] = R[j]; 
+        j++; 
+        k++; 
+    } 
+} 
+
 // implement merge sort
 // extraMemoryAllocated counts bytes of extra memory allocated
 void mergeSort(int pData[], int l, int r)
 {
+    if(l < r){
+        // set mid to --> ( left + right-1 ) / 2
+        int m = l + (r - l) / 2; 
+        
+        // Sort first and second halves 
+        mergeSort(pData, l, m); 
+        mergeSort(pData, m + 1, r); 
+
+        merge(pData, l, m, r);
+    }
+
 }
 
 // parses input file to an integer array
